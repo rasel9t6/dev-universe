e69a0b465d06806a6c46c358c08d09868d9f2d55
Rasel Hossen <raselhossen052@gmail.com>
2023-11-05 17:17:03 +0600

    tag-functionality-inplemented

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -16,6 +16,8 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { QuestionsSchema } from '@/lib/validations';
+import { Badge } from '../ui/badge';
+import Image from 'next/image';
 
 const Question = () => {
   const editorRef = useRef(null);
@@ -28,9 +30,38 @@ const Question = () => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof QuestionsSchema>) {
-    console.log(values);
-  }
+  function onSubmit(values: z.infer<typeof QuestionsSchema>) {}
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: any
+  ) => {
+    if (e.key === 'Enter' && field.name === 'tags') {
+      e.preventDefault();
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+      if (tagValue !== '') {
+        if (tagValue.length > 15) {
+          return form.setError('tags', {
+            type: 'required',
+            message: 'Tag must be less than 15 characters.',
+          });
+        }
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue('tags', [...field.value, tagValue]);
+          tagInput.value = '';
+          form.clearErrors('tags');
+        }
+      } else {
+        form.trigger();
+      }
+    }
+  };
+
+  const handelTagRemove = (tag: string, field: any) => {
+    const newTags = field.value.filter((item: string) => item !== tag);
+    form.setValue('tags', newTags);
+  };
+
   return (
     <Form {...form}>
       <form
@@ -80,9 +111,21 @@ const Question = () => {
                     height: 350,
                     menubar: false,
                     plugins: [
-                      'advlist', 'autolink', 'lists' ,'link' ,'image' ,'charmap' ,'preview', 'anchor',
-                      'searchreplace' ,'visualblocks','codesample' ,'fullscreen',
-                      'insertdatetime', 'media', 'table',
+                      'advlist',
+                      'autolink',
+                      'lists',
+                      'link',
+                      'image',
+                      'charmap',
+                      'preview',
+                      'anchor',
+                      'searchreplace',
+                      'visualblocks',
+                      'codesample',
+                      'fullscreen',
+                      'insertdatetime',
+                      'media',
+                      'table',
                     ],
                     toolbar:
                       'undo redo | ' +
@@ -110,11 +153,33 @@ const Question = () => {
                 Tags <span className='text-primary-500'>*</span>
               </FormLabel>
               <FormControl className='mt-3.5'>
-                <Input
-                  className='no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border'
-                  {...field}
-                  placeholder='Add tags...'
-                />
+                <>
+                  <Input
+                    className='no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border'
+                    placeholder='Add tags...'
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+                  {field.value.length > 0 && (
+                    <div className='flex-start mt-2.5 gap-2.5'>
+                      {field.value.map((tag: any) => (
+                        <Badge
+                          key={tag}
+                          className='subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize'
+                          onClick={() => handelTagRemove(tag, field)}
+                        >
+                          {tag}
+                          <Image
+                            src='/assets/icons/close.svg'
+                            alt='Close icon'
+                            width={12}
+                            height={12}
+                            className='cursor-pointer object-contain invert-0 dark:invert'
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
               <FormDescription className='body-regular mt-2.5 text-light-500'>
                 Add up to 3 tags to describe what your question is about. You
