ccea015c29129f0944e7af9b14eb1a615b6ac49f
Rasel Hossen <raselhossen052@gmail.com>
2023-11-08 21:33:17 +0600

    authentication bug fixes

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,21 +1,22 @@
-import React from "react";
-import Question from "@/components/forms/Question";
-// import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
-import { getUserById } from "@/lib/actions/user.action";
+import React from 'react';
+import Question from '@/components/forms/Question';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import { getUserById } from '@/lib/actions/user.action';
 const Page = async () => {
-  // const userId = auth();
-  const userId = 'yourClerkId';
-  if(!userId) redirect('/sign-in')
-  const mongoUser = await getUserById({ userId })
+  const { userId } = auth();
 
-  
-  return <div>
-    <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
-    <div className="mt-9">
-      <Question mongoUserId={ JSON.stringify(mongoUser)} />
+  if (!userId) redirect('/sign-in');
+  const mongoUser = await getUserById({ userId });
+
+  return (
+    <div>
+      <h1 className='h1-bold text-dark100_light900'>Ask a question</h1>
+      <div className='mt-9'>
+        <Question mongoUserId={JSON.stringify(mongoUser)} />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Page;
